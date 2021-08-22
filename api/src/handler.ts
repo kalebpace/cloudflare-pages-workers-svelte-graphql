7c5a855b5f50066863d4a0d94c192a8979ae9225
Kaleb Pace <kaleb.pace@pm.me>
2021-08-22 22:24:35 +0000

    fix(api): cors tweaking

--- api/src/handler.ts
+++ api/src/handler.ts
@@ -7,7 +7,7 @@ const corsHeaders = {
 async function handleRequest(request: Request): Promise<Response> {
   const headers = new Headers({
       ...corsHeaders,
-      "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers") || "*",
+      "Access-Control-Allow-Headers": "*",
     })
   const response = new Response(JSON.stringify({msg:`Hello from Workers, [${request.url}]!`}), {
     headers: headers
@@ -16,32 +16,22 @@ async function handleRequest(request: Request): Promise<Response> {
 }
 
 async function handleOptions(request: Request): Promise<Response> {
-  // Make sure the necessary headers are present
-  // for this to be a valid pre-flight request
   const headers = request.headers;
   if (
     headers.get("Origin") !== null &&
     headers.get("Access-Control-Request-Method") !== null &&
     headers.get("Access-Control-Request-Headers") !== null
   ){
-    // Handle CORS pre-flight request.
-    // If you want to check or reject the requested method + headers
-    // you can do that here.
     const respHeaders = {
       ...corsHeaders,
-    // Allow all future content Request headers to go back to browser
-    // such as Authorization (Bearer) or X-Client-Name-Version
       "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers"),
     }
-
 
     return new Response(null, {
       headers: respHeaders as HeadersInit,
     })
   }
   else {
-    // Handle standard OPTIONS request.
-    // If you want to allow other HTTP Methods, you can do that here.
     return new Response(null, {
       headers: {
         Allow: "GET, HEAD, POST, OPTIONS",
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,25 +2,22 @@ import { handleRequest, handleOptions } from './handler'
 
 addEventListener('fetch', (event) => {
   const request = event.request
-  event.respondWith(handleRequest(request))
-  // if (request.method === "OPTIONS") {
-  //   // Handle CORS preflight requests
-  //   event.respondWith(handleOptions(request))
-  // }
-  // else if(
-  //   request.method === "GET" ||
-  //   request.method === "HEAD" ||
-  //   request.method === "POST"
-  // ){
-  //   // Handle requests to the API server
-  //   event.respondWith(handleRequest(request))
-  // }
-  // else {
-  //   event.respondWith(
-  //     new Response(null, {
-  //       status: 405,
-  //       statusText: "Method Not Allowed",
-  //     }),
-  //   )
-  // }
+  if (request.method === "OPTIONS") {
+    event.respondWith(handleOptions(request))
+  }
+  else if(
+    request.method === "GET" ||
+    request.method === "HEAD" ||
+    request.method === "POST"
+  ){
+    event.respondWith(handleRequest(request))
+  }
+  else {
+    event.respondWith(
+      new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      }),
+    )
+  }
 })
