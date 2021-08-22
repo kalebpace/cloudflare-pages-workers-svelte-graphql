c100ca0cac878bc4b8fdc16265d2a335eadd4c8f
Kaleb Pace <kaleb.pace@pm.me>
2021-08-22 21:20:07 +0000

    feat(api): add cors support

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,25 @@
-import { handleRequest } from './handler'
+import { handleRequest, handleOptions } from './handler'
 
 addEventListener('fetch', (event) => {
-  return event.respondWith(handleRequest(event.request))
+  const request = event.request
+  if (request.method === "OPTIONS") {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(request))
+  }
+  else if(
+    request.method === "GET" ||
+    request.method === "HEAD" ||
+    request.method === "POST"
+  ){
+    // Handle requests to the API server
+    event.respondWith(handleRequest(request))
+  }
+  else {
+    event.respondWith(
+      new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      }),
+    )
+  }
 })
--- api/test/handler.test.ts
+++ api/test/handler.test.ts
@@ -12,7 +12,8 @@ describe('handle', () => {
   test('handle GET', async () => {
     const result = await handleRequest(new Request('/', { method: 'GET' }))
     expect(result.status).toEqual(200)
-    const text = await result.text()
-    expect(text).toEqual('request method: GET')
+
+    const actual = await result.json()
+    expect(actual.msg).toContain('Hello from Workers')
   })
 })
