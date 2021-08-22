69edb15d05d980fa8680802ffc35dfe74f192ecb
Kaleb Pace <kaleb.pace@pm.me>
2021-08-22 21:43:39 +0000

    fix(api): skip preflight cors logic for testing

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,24 +2,25 @@ import { handleRequest, handleOptions } from './handler'
 
 addEventListener('fetch', (event) => {
   const request = event.request
-  if (request.method === "OPTIONS") {
-    // Handle CORS preflight requests
-    event.respondWith(handleOptions(request))
-  }
-  else if(
-    request.method === "GET" ||
-    request.method === "HEAD" ||
-    request.method === "POST"
-  ){
-    // Handle requests to the API server
-    event.respondWith(handleRequest(request))
-  }
-  else {
-    event.respondWith(
-      new Response(null, {
-        status: 405,
-        statusText: "Method Not Allowed",
-      }),
-    )
-  }
+  event.respondWith(handleRequest(request))
+  // if (request.method === "OPTIONS") {
+  //   // Handle CORS preflight requests
+  //   event.respondWith(handleOptions(request))
+  // }
+  // else if(
+  //   request.method === "GET" ||
+  //   request.method === "HEAD" ||
+  //   request.method === "POST"
+  // ){
+  //   // Handle requests to the API server
+  //   event.respondWith(handleRequest(request))
+  // }
+  // else {
+  //   event.respondWith(
+  //     new Response(null, {
+  //       status: 405,
+  //       statusText: "Method Not Allowed",
+  //     }),
+  //   )
+  // }
 })
